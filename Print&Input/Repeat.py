820bd6056136cb87ccc2374bb04677e553b1d000
Kasaru <58173553+Kasaru@users.noreply.github.com>
2022-11-04 22:06:56 +0400

    Print&Input2

---
+++ Print&Input/Repeat.py
@@ -0,0 +1,6 @@
+name1 = input()
+name2 = input()
+name3 = input()
+print(name1)
+print(name2)
+print(name3)
---
+++ Print&Input/Repeat2.py
@@ -0,0 +1,6 @@
+name1 = input()
+name2 = input()
+name3 = input()
+print(name3)
+print(name2)
+print(name1)
