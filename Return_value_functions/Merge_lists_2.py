52e4df5a970769e6f63a4947cf6c049357bf36b1
Kasaru <58173553+Kasaru@users.noreply.github.com>
2023-01-22 15:46:05 +0400

    Merge_lists_2 На вход программе подается число � n, а затем � n строк, содержащих целые числа в порядке возрастания. Из данных строк формируются списки чисел. Напишите программу, которая объединяет указанные списки в один отсортированный список с помощью функции quick_merge(), а затем выводит его.
    Формат входных данных
    На вход программе подается натуральное число
    �
    n, а затем
    �
    n строк, содержащих целые числа в порядке возрастания, разделенные символом пробела.
    
    Формат выходных данных
    Программа должна вывести текст в соответствии с условием задачи.

---
+++ Return_value_functions/Merge_lists_2.py
@@ -0,0 +1,26 @@
+def quick_merge(list1, list2):
+    result = []
+
+    p1 = 0  # указатель на первый элемент списка list1
+    p2 = 0  # указатель на первый элемент списка list2
+
+    while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+        if int(list1[p1]) <= int(list2[p2]):
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append(list2[p2])
+            p2 += 1
+
+    if p1 < len(list1):  # прицепление остатка
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+
+    return result
+l = []
+n = int(input())
+for i in range(n):
+    list1 = input().split(" ")
+    l = quick_merge(l, list1)
+print(*l)
