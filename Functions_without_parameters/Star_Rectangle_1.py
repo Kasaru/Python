8bdc19269d3496d0565a3e5b7101b84d9c578b33
Kasaru <58173553+Kasaru@users.noreply.github.com>
2023-01-22 15:32:30 +0400

    Star_Rectangle_1 Напишите функцию draw_box(), которая выводит звездный прямоугольник с размерами 14 × 10 14×10 в соответствии с образцом:
    **********
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    *        *
    **********
    Примечание. Для вывода прямоугольника используйте цикл for.

---
+++ Functions_without_parameters/Star_Rectangle_1.py
@@ -0,0 +1,8 @@
+def draw_box():
+    for i in range(14):
+        if i == 0 or i == 13:
+            print("**********")
+        else:
+            print("*        *")
+# основная программа
+draw_box()  # вызов функции
